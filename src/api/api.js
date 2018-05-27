155070ae5eec05e919acc6d34030a96d2fce5e18
xxpstyle <905419850@qq.com>
2018-05-27 12:05:50 +0800

    实现vue项目与web后台进行数据交互

---
+++ src/api/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+let base = '';
+let instance = axios.create({
+   headers: {'content-type': 'application/x-www-form-urlencoded'},
+   withCredentials: true
+
+ })
+
+export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
+
+export const getUserList = params => { return axios.get(`${base}/user/list`, { params: params }); };
+
+export const getUserListPage = params => { return axios.get(`${base}/user/listpage`, { params: params }); };
+
+export const removeUser = params => { return axios.get(`${base}/user/remove`, { params: params }); };
+
+export const batchRemoveUser = params => { return axios.get(`${base}/user/batchremove`, { params: params }); };
+
+export const editUser = params => { return axios.get(`${base}/user/edit`, { params: params }); };
+
+export const addUser = params => { return axios.get(`${base}/user/add`, { params: params }); };
+
+export const getTestByHT = params => { return axios.get(`http://localhost:9090/test/test`, { params: params }); };
